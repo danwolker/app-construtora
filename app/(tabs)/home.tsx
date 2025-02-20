d47e0c855d145a2fab9d1b5e80e111e7d8d781d1
Laila <lailarhcampos@gmail.com>
2025-02-19 22:59:18 -0300

    Adiciona tela inicial com componente HomeScreen

---
+++ app/(tabs)/home.tsx
@@ -0,0 +1,12 @@
+import HomeScreen from '@/screens/HomeScreen/HomeScreen'
+
+const Home = () => {
+  return (
+    <>
+      {/* <StatusBar barStyle="light-content" translucent backgroundColor="transparent" /> */}
+      <HomeScreen />
+    </>
+  )
+}
+
+export default Home
---
+++ screens/HomeScreen/HomeScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const HomeScreen = () => {
+  return (
+    <View className='flex-1 items-center justify-center'>
+      <Text className='text-5xl'>Home Screen</Text>
+    </View>
+  )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({})
